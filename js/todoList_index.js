4d2a9b91e95eab4c1197ecf4bccb2e0368c68231
KC2A3 <947496351@qq.com>
2019-01-09 02:50:32 +0800

    Get previously saved events

--- js/todoList_index.js
+++ js/todoList_index.js
@@ -13,3 +13,26 @@ function addTodolist() {
   document.getElementById("add_list").value = "";
   load();
 }
+
+function load() {
+  var todo = document.getElementById("todolist");
+  var done = document.getElementById("donelist");
+  var todoString = "";
+  var doneString = "";
+  document.getElementById("add_list").focus();
+  todolist = loadData();
+  if (todolist != null) {
+    for (var i = 0; i < todolist.length; i++) {
+      if (!todolist[i].done) {
+        todoString += "<li>" + "<input type='checkbox' onchange='update(" + i + ", \"done\", true)'>" + "<p id='p-" + i + "' onclick='changeType(" + i + ")'>" + todolist[i].todo + "</p>" + "<a onclick='remove(" + i + ")'>×</a>" + "</li>";
+      } else {
+        doneString += "<li>" + "<input type='checkbox' " + "onchange='update(" + i + ", \"done\", false)' checked>" + "<p id='p-" + i + "' onclick='changeType(" + i + ")'>" + todolist[i].todo + "</p>" + "<a onclick='remove(" + i + ")'>×</a>" + "</li>";
+      }
+    }
+    todo.innerHTML = todoString;
+    done.innerHTML = doneString;
+  } else {
+    todo.innerHTML = "";
+    done.innerHTML = "";
+  }
+}
